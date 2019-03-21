83c4734d65fb3557c3f4b56f5509b1d7af7a5665
Brandon Henricks <brandon.henricks1984@gmail.com>
2019-03-21 18:58:31 -0400

    Initial Code Commit
    Initial Code Commit

---
+++ HCI.AspNetCore.Extensions/HCI.AspNetCore.Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace HCI.AspNetCore.Extensions
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+
+    /// <summary>
+    /// Application builder extensions.
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Uses the mvc with additional routes.
+        /// </summary>
+        /// <returns>The mvc with additional routes.</returns>
+        /// <param name="builder">Builder.</param>
+        public static IApplicationBuilder UseMvcWithAdditionalRoutes(this IApplicationBuilder builder)
+        {
+            builder.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{?id}");
+
+                routes.MapRoute(
+                    "areaRoute",
+                    "{area}/{controller=Home}/{action=Index}/{?id}"
+                );
+            });
+
+            return builder;
+        }
+
+        public static IApplicationBuilder AddSPAFallBack(this IApplicationBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
---
+++ HCI.AspNetCore.Extensions/HCI.AspNetCore.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,60 @@
+﻿
+namespace HCI.AspNetCore.Extensions
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Versioning;
+    using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
+
+    /// <summary>
+    /// Service collection extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the custom API versioning. With UrlSegment, Default Version Selector.
+        /// </summary>
+        /// <returns>The custom API versioning.</returns>
+        /// <param name="services">Services.</param>
+        /// <param name="convention">Convention.</param>
+        /// https://github.com/Microsoft/aspnet-api-versioning/wiki/API-Version-Conventions
+        public static IServiceCollection AddCustomApiVersioning(this IServiceCollection services, IControllerConvention convention = null)
+        {
+            if (services is null) throw new ArgumentNullException(nameof(services));
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+
+                if (convention != null)
+                {
+                    options.Conventions.Add(convention);
+                }
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Configures the cors to Allow All
+        /// </summary>
+        /// <param name="services">Services.</param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+    }
+}
